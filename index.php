c36c0727913acbbc1f95996b730ef54188a8f1cf
Mark McKnight <mark.mcknight@yale.edu>
2021-01-27 16:37:18 -0500

    Moved style.css to root as wordpress expects.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/css/styles.css?v=1.0">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>style.css?v=1.0">
 
 </head>
 <body>
