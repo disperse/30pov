497c91f83a5c37167515f466f32334627401a743
Mark McKnight <mark.mcknight@yale.edu>
2021-02-01 10:47:43 -0500

    Removed justify from author descriptions, added justify to posts.

--- author-card.php
+++ author-card.php
@@ -3,7 +3,7 @@
   <div class="card-body">
     <img style="float:right; margin-left: 1em; max-width: 96px; height: auto;" src="<?php echo get_avatar_url($author_id) ?>" alt="<?php echo get_the_author_meta('display_name', $author_id) ?>"/>
     <em>About <?php echo get_the_author_meta('display_name', $author_id); ?></em>
-    <p style="margin-top: 5px; text-align: justify;">
+    <p style="margin-top: 5px;">
     <?php the_author_meta('description', $author_id); ?>
 </p>
 <?php if (! $args['is_single_author']): ?>
