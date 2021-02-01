af6680c61af08d90fcfb15a3fd50536aad835b37
Mark McKnight <mark.mcknight@yale.edu>
2021-02-01 14:30:13 -0500

    Hide navigation on pages.

--- navigation.php
+++ navigation.php
@@ -1,4 +1,6 @@
 <div style="display: flex; margin: 10px;">
+  <?php if (! is_page()): ?>
   <span><?php previous_post_link('&laquo; %link', 'Previous'); ?></span>
   <span style="margin-left: auto;"><?php next_post_link('%link &raquo; ', 'Next'); ?></span>
+  <?php endif; ?>
 </div>
